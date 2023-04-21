d4500805d97004246c7b1a626ac534ac23472f03
giovannilampis <giolampis@hotmail.com>
2023-04-21 16:01:39 +0200

    create 10 email addresses at a time

--- js/script.js
+++ js/script.js
@@ -6,26 +6,28 @@ const { createApp } = Vue
 createApp ({
     data() {
         return {
+            // randomly generated emails will be storewd in this array
             mailingList: []
         }
+    },
+
+    methods: {
+        // this function allows to get a randomly generated email from an API
+        callBooleanApi(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (res) => {
+                    // Push a randomly generated email to
+                    this.mailingList.push(res.data.response)
+                })
+            
+            }
+           
+        }
+
     },
 
     created(){
-        
        
-    },
-
-
-    methods: {
-
-        callBooleanApi(){
-
-            mailingList: []
-
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (res) => {
-                this.mailingList.push(res.data.response)
-            })
-        }
-
     }
+
 }).mount("#app")
