8d533ee17b87108a20e0699d30d51b0f5738b62f
giovannilampis <giolampis@hotmail.com>
2023-04-21 14:59:48 +0200

    add callBooleanApi() to methods

--- js/script.js
+++ js/script.js
@@ -12,14 +12,18 @@ createApp ({
 
     created(){
         
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(response){
-            // console.log( response );
-            this.mailingList.push(response.data.response)
-        })
+       
     },
 
 
     methods: {
+
+        callBooleanApi(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function(response){
+                // console.log( response );
+                this.mailingList.push(response.data.response)
+            })
+        }
 
     }
 }).mount("#app")
